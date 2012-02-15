bd6cf429f0db5330e4a37ef5688ced0261cb4ef6
alambike <alambike@inspiron.(none)>
2012-02-15 19:14:13 +0100

    twitapp-scaf

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demoapp::Application.config.secret_token = '644ac64b977fa3edd73d5f8b7217b791b47e0addc6390339be6b8b6b9f360e659151e41598f8e7193192bf1b82df807af6c71a3668014e7b8a48b296a0f07645'
